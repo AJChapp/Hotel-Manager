d4d313c0723cd3ee2cac8392ef1089e35f5c9622
AJC <drew.chappetta@gmail.com>
2018-11-13 19:43:06 -0600

    .

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(passport.session());
 genRoutes(app);
 loginRoutes(app, passport);
 PassPort(passport);
-app.use(express.static('client/public')); 
+app.use(express.static('client/build/public')); 
 
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/project3'
 mongoose.connect(mongoURI);
